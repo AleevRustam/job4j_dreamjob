3ed6c985d5f80b18e4b49d2be4ae00c99f2b7031
AleevRustam <re1973@mail.ru>
2025-04-14 12:19:00 +0200

    3. Тестирование репозиториев [#504862]

--- src/main/java/ru/job4j/dreamjob/repository/Sql2oCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oCandidateRepository.java
@@ -20,7 +20,7 @@ public class Sql2oCandidateRepository implements CandidateRepository {
     public Candidate save(Candidate candidate) {
         try (var connection = sql2o.open()) {
             var sql = "INSERT INTO candidates(name, description, creation_date, city_id, file_id) "
-            + "VALUES (:name, :description, :creationDate, :cityId, :fileId)";
+                    + "VALUES (:name, :description, :creationDate, :cityId, :fileId)";
 
             var query = connection.createQuery(sql, true)
                     .addParameter("name", candidate.getName())
