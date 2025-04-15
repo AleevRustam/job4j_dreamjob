a87a215893ff0187c3202fe1e50960f91a16f4a7
AleevRustam <re1973@mail.ru>
2025-04-15 13:22:52 +0200

    4. Многопоточность в базе данных [#504860]

---
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -0,0 +1,45 @@
+package ru.job4j.dreamjob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.dreamjob.model.User;
+import ru.job4j.dreamjob.service.UserService;
+
+import javax.annotation.concurrent.ThreadSafe;
+
+@Controller
+@ThreadSafe
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String getRegistrationPage() {
+        return "users/register";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "users/login";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute User user, Model model) {
+        try {
+            userService.save(user);
+            return "redirect:/users/login";
+        } catch (Exception exception) {
+            model.addAttribute("message", exception.getMessage());
+            return "errors/404";
+        }
+    }
+}
